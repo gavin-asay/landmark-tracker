cbb92d1e297212cc3badf360ded3a0f9349668dc
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-02-24 21:01:37 -0700

    added simple seeds for landmarks and users.

--- public/javascript/add_landmark.js
+++ public/javascript/add_landmark.js
@@ -1,14 +1,21 @@
 let locationsArr = [];
 let currentLocation = "";
+let locationObj = {};
 
 //this actively tracks users location. Anytime it changes, the new location gets pushed to locationArr array, and set as currentLocation. The landmarks lat and lon can then be set with currentLocation.latitude or longitude
 navigator.geolocation.watchPosition(
   (data) => {
     locationsArr.push(data);
     currentLocation = data.coords;
-    console.log(data);
+    locationObj = {
+      arr: locationsArr,
+      current: currentLocation,
+    };
     console.log(locationsArr);
     console.log(currentLocation);
+    console.log(locationObj);
   },
   (err) => console.log(err)
 );
+
+module.exports = locationObj;
