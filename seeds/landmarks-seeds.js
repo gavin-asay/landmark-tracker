9f2063b9c9a0c065b7020938b14fe38edd05334e
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-02-26 21:54:55 -0700

    added user routes. put requests not working; problem between sessions and insomnia i think. also added delete request for landmarks

--- seeds/landmarks-seeds.js
+++ seeds/landmarks-seeds.js
@@ -1,17 +1,18 @@
-const { Landmark } = require('../models');
+const { Landmark } = require("../models");
 
 const locationdata = [
-	{
-		name: 'some cool place',
-		address: '123 south 456 east',
-		lat: 40.7608,
-		lon: 111.891,
-	},
-	{
-		name: 'Orem City Library',
-		lat: 40.280628,
-		lon: -111.716139,
-	},
+  {
+    name: "some cool place",
+    address: "123 south 456 east",
+    lat: 40.7608,
+    lon: 111.891,
+    added_by: 1,
+  },
+  {
+    name: "Orem City Library",
+    lat: 40.280628,
+    lon: -111.716139,
+  },
 ];
 
 const seedLandmarks = () => Landmark.bulkCreate(locationdata);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -6,6 +6,11 @@ const userdata = [
     email: "test@email.com",
     password: "test1234",
   },
+  {
+    username: "test2",
+    email: "email@email.com",
+    password: "password",
+  },
 ];
 
 const seedUsers = () => User.bulkCreate(userdata);
