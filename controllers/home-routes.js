389a15ec564c5f8d062948f95380dbbd60eed812
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-03-01 20:00:56 -0700

    finished the homepage route i think.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,9 +3,30 @@ const sequelize = require("../config/connection");
 const { User, Landmark } = require("../models");
 
 router.get("/", (req, res) => {
-  res.render("homepage", {
-    loggedIn: req.session.loggedIn,
-  });
+  Landmark.findAll({
+    attributes: [
+      "id",
+      "name",
+      "address",
+      "lat",
+      "lon",
+      "image_url",
+      "added_by",
+    ],
+  })
+    .then((dbLandmarkData) => {
+      const landmarks = dbLandmarkData.map((landmark) =>
+        landmark.get({ plain: true })
+      );
+      console.log(landmarks);
+      res.render("homepage", {
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
