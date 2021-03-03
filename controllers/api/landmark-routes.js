2964c06ae980d3631ac7939a8935935d971f0935
Gavin Asay <gavin.asay@gmail.com>
2021-03-03 03:40:52 -0700

    improved map-marker

--- controllers/api/landmark-routes.js
+++ controllers/api/landmark-routes.js
@@ -1,126 +1,120 @@
-const router = require("express").Router();
-const { Landmark } = require("../../models");
-const { latRange, lonRange } = require("../../utils/distance");
-const { Op } = require("sequelize");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const { Landmark } = require('../../models');
+const { latRange, lonRange } = require('../../utils/distance');
+const { Op } = require('sequelize');
+const withAuth = require('../../utils/auth');
 
-router.get("/:user_lat/:user_lon", async (req, res) => {
-  // get all landmarks in an area
-  req.params.user_lat = parseFloat(req.params.user_lat);
-  req.params.user_lon = parseFloat(req.params.user_lon);
+router.get('/:user_lat/:user_lon', async (req, res) => {
+	// get all landmarks in an area
+	req.params.user_lat = parseFloat(req.params.user_lat);
+	req.params.user_lon = parseFloat(req.params.user_lon);
 
-  try {
-    const dbLandmarkData = await Landmark.findAll({
-      where: {
-        [Op.and]: [
-          {
-            lat: {
-              [Op.between]: latRange(req.params.user_lat, 5),
-            },
-          },
-          {
-            lon: {
-              [Op.between]: lonRange(
-                req.params.user_lat,
-                req.params.user_lon,
-                5
-              ),
-            },
-          },
-        ],
-      },
-      user_lat: req.params.user_lat,
-      user_lon: req.params.user_lon,
-    });
-    if (!dbLandmarkData) {
-      res.status(404).json({ message: "No landmarks found in this area" });
-      return;
-    }
+	try {
+		const dbLandmarkData = await Landmark.findAll({
+			where: {
+				[Op.and]: [
+					{
+						lat: {
+							[Op.between]: latRange(req.params.user_lat, 5),
+						},
+					},
+					{
+						lon: {
+							[Op.between]: lonRange(req.params.user_lat, req.params.user_lon, 5),
+						},
+					},
+				],
+			},
+			user_lat: parseFloat(req.params.user_lat),
+			user_lon: parseFloat(req.params.user_lon),
+		});
+		if (!dbLandmarkData) {
+			res.status(404).json({ message: 'No landmarks found in this area' });
+			return;
+		}
 
-    res.json(dbLandmarkData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		res.json(dbLandmarkData);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.get("/:id", async (req, res) => {
-  // get single landmark, likely for pulling up user's saved favorites?
-  try {
-    const dbLandmarkData = await Landmark.findByPk(req.params.id);
+router.get('/:id', async (req, res) => {
+	// get single landmark, likely for pulling up user's saved favorites?
+	try {
+		const dbLandmarkData = await Landmark.findByPk(req.params.id);
 
-    dbLandmarkData
-      ? res.json(dbLandmarkData)
-      : res.status(404).json({ message: "No landmark under this id" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		dbLandmarkData ? res.json(dbLandmarkData) : res.status(404).json({ message: 'No landmark under this id' });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.post("/", async (req, res) => {
-  // create new landmark
-  try {
-    const dbLandmarkData = await Landmark.create({
-      name: req.body.name,
-      address: req.body.address,
-      lat: req.body.lat,
-      lon: req.body.lon,
-      image_url: req.body.image_url,
-      added_by: req.session.user_id,
-    });
+router.post('/', async (req, res) => {
+	// create new landmark
+	try {
+		const dbLandmarkData = await Landmark.create({
+			name: req.body.name,
+			address: req.body.address,
+			lat: req.body.lat,
+			lon: req.body.lon,
+			image_url: req.body.image_url,
+			added_by: req.session.user_id,
+		});
 
-    res.json(dbLandmarkData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		res.json(dbLandmarkData);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.put("/:id", async (req, res) => {
-  // likely used for adding missing data, like an image or address
-  try {
-    const dbLandmarkData = await Landmark.update(
-      {
-        name: req.body.name,
-        address: req.body.address,
-        lat: req.body.lat,
-        lon: req.body.lon,
-        image_url: req.body.image_url,
-        added_by: req.session.user_id,
-      },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
+router.put('/:id', async (req, res) => {
+	// likely used for adding missing data, like an image or address
+	try {
+		const dbLandmarkData = await Landmark.update(
+			{
+				name: req.body.name,
+				address: req.body.address,
+				lat: req.body.lat,
+				lon: req.body.lon,
+				image_url: req.body.image_url,
+				added_by: req.session.user_id,
+			},
+			{
+				where: {
+					id: req.params.id,
+				},
+			}
+		);
 
-    res.json(dbLandmarkData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		res.json(dbLandmarkData);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.delete("/:id", (req, res) => {
-  console.log("id", req.params.id);
-  Landmark.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbLandmarkData) => {
-      if (!dbLandmarkData) {
-        res.status(404).json({ message: "No landmark found with this id" });
-        return;
-      }
-      res.json(dbLandmarkData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.delete('/:id', (req, res) => {
+	console.log('id', req.params.id);
+	Landmark.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then(dbLandmarkData => {
+			if (!dbLandmarkData) {
+				res.status(404).json({ message: 'No landmark found with this id' });
+				return;
+			}
+			res.json(dbLandmarkData);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
--- public/javascript/map-markers.js
+++ public/javascript/map-markers.js
@@ -1,43 +1,90 @@
 let map;
 let currentCoords = {};
+let markers = [];
 
-function success(pos) {
-	console.log(pos.coords);
-	currentCoords.lat = pos.coords.latitude;
-	currentCoords.lng = pos.coords.longitude;
-}
+const haversineDistance = ([lat1, lon1], [lat2, lon2], isMiles = true) => {
+	const toRadian = angle => (Math.PI / 180) * angle;
+	const distance = (a, b) => (Math.PI / 180) * (a - b);
+	const RADIUS_OF_EARTH_IN_KM = 6371;
 
-function getPosition() {
-	if (!map) {
-		return new Promise((resolve, reject) => {
-			navigator.geolocation.getCurrentPosition(resolve, reject);
-		});
+	const dLat = distance(lat2, lat1);
+	const dLon = distance(lon2, lon1);
+
+	lat1 = toRadian(lat1);
+	lat2 = toRadian(lat2);
+
+	// Haversine Formula
+	const a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+	const c = 2 * Math.asin(Math.sqrt(a));
+
+	let finalDistance = RADIUS_OF_EARTH_IN_KM * c;
+
+	if (isMiles) {
+		finalDistance /= 1.60934;
 	}
-}
-
-function setMapPosition(pos) {
-	map.setCenter({ lat: pos.coords.latitude, lng: pos.coords.longitude });
-}
+	console.log([lat1, lon1], [lat2, lon2]);
+	return finalDistance;
+};
 
 async function initMap() {
+	async function getLandmarks(lat, lon) {
+		const response = await fetch(`/api/landmarks/${lat}/${lon}`);
+		const landmarks = await response.json();
+
+		for (let i = 0; i < landmarks.length; i++) {
+			const { lat, lon, name } = landmarks[i];
+			const marker = new google.maps.Marker({
+				position: { lat, lng: lon },
+				map,
+				title: name,
+			});
+			markers.push(marker);
+		}
+	}
+
+	function getPosition() {
+		return new Promise((resolve, reject) => {
+			// if we can't get location data, reject promise
+			if (!navigator.geolocation) {
+				reject('Location not available');
+				return;
+			}
+
+			navigator.geolocation.getCurrentPosition(pos => {
+				if (!map) resolve(pos);
+				// if map isn't made yet, get position for map center
+				// otherwise, record current location
+				else {
+					if (haversineDistance([pos.coords.latitude, pos.coords.longitude], [currentCoords.lon, currentCoords.lng]) > 0.095) {
+						// if position changes by 0.095 mi (about 500 ft), fetch landmarks for new position
+						currentCoords.lat = pos.coords.latitude;
+						currentCoords.lng = pos.coords.longitude;
+
+						for (let i = 0; i < markers.length; i++) {
+							markers[i].setMap(null);
+						}
+						markers = [];
+
+						getLandmarks(currentCoords.lat, currentCoords.lng);
+					} else {
+						currentCoords.lat = pos.coords.latitude;
+						currentCoords.lng = pos.coords.longitude;
+					}
+				}
+			});
+		});
+	}
+
 	const pos = await getPosition();
+	if (pos === 'Location not available') {
+		document.querySelector('#map').textContent = pos;
+		return;
+	}
 
 	map = new google.maps.Map(document.querySelector('#map'), {
 		center: { lat: pos.coords.latitude, lng: pos.coords.longitude },
 		zoom: 16,
 	});
 
-	const response = await fetch(`/api/landmarks/${pos.coords.latitude}/${pos.coords.longitude}`);
-	const landmarks = await response.json();
-
-	for (let i = 0; i < landmarks.length; i++) {
-		const { lat, lon, name } = landmarks[i];
-		new google.maps.Marker({
-			position: { lat, lng: lon },
-			map,
-			title: name,
-		});
-	}
-
-	navigator.geolocation.watchPosition(setMapPosition);
+	getLandmarks(pos.coords.latitude, pos.coords.longitude);
 }
